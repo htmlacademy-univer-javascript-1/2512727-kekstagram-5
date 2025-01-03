f59eb7187a9b1dda3e953ef07c8f5e973bd9b6af
Булатова Дарья <bulatovadasha2005@gmail.com>
2025-01-04 00:07:22 +0500

    8.15. Открывается и закрывается (часть 2)

--- js/data.js
+++ js/data.js
@@ -1,66 +1,40 @@
-import {getRandomInteger, getRandomArrayElement} from './util.js';
+import {
+  createIdGenerator,
+  getRandomInteger,
+  getRandomArrayElement,
+} from './utils.js';
 
-const PICTURE_COUNT = 25;
-const LIKE_MIN_COUNT = 15;
-const LIKE_MAX_COUNT = 200;
-const COMMENT_COUNT = 30;
-const AVATAR_COUNT = 6;
-const DESCRIPTIONS = [
-  'Моменты, которые запечатлены навсегда',
-  'Вся красота мира в одной картинке',
-  'Счастье в каждом кадре',
-  'Когда слова не нужны, достаточно фотографии',
-  'История, рассказанная через объектив',
-  'Остановить время в одном кадре',
-  'Сегодня — самый лучший день',
-  'Я не доверяю словам. Я доверяю фотографиям',
-  'Момент, когда небо и земля сливаются воедино',
-  'Фотографии — это свидетельство о том, что мы жили',
-];
-const COMMENT_LINES = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-];
-const NAMES = ['Андрей', 'Артем', 'Катя', 'Паша', 'Вадим', 'Маша', 'Стас', 'Даша'];
-
-const createIdGenerator = () => {
-  let lastGenerateId = 0;
-
-  return () => {
-    lastGenerateId += 1;
-    return lastGenerateId;
-  };
-};
+import {
+  AVATAR_COUNT,
+  PICTURE_DESCRIPTIONS,
+  LIKES_COUNT,
+  NAMES,
+  COMMENT_MESSAGES,
+  COMMENTS_COUNT,
+  PICTURES_COUNT
+} from './constants.js';
 
 const generateCommentId = createIdGenerator();
 
-const createMessage = () => Array.from (
-  {length: getRandomInteger(1, 2) },
-  () => getRandomArrayElement(COMMENT_LINES)).join(' ');
-
-const createComment = () => ({
+export const createComment = () => ({
   id: generateCommentId(),
   avatar: `img/avatar-${getRandomInteger(1, AVATAR_COUNT)}.svg`,
-  message: createMessage(),
+  message: getRandomArrayElement(COMMENT_MESSAGES),
   name: getRandomArrayElement(NAMES),
 });
 
-const createPicture = (index) => ({
-  id: index,
-  url: `photos/${index}.jpg`,
-  description: getRandomArrayElement(DESCRIPTIONS),
-  likes: getRandomInteger(LIKE_MIN_COUNT, LIKE_MAX_COUNT),
-  comments: Array.from (
-    { length: getRandomInteger(0, COMMENT_COUNT) },
-    createComment,
+export const createPicture = (id) => ({
+  id,
+  url: `photos/${id}.jpg`,
+  description: getRandomArrayElement(PICTURE_DESCRIPTIONS),
+  likes: getRandomInteger(LIKES_COUNT.MIN, LIKES_COUNT.MAX),
+  comments: Array.from(
+    { length: getRandomInteger(0, COMMENTS_COUNT) },
+    createComment
   ),
 });
 
-const getPicture = () => Array.from (
-  { length: PICTURE_COUNT },
+export const getPictures = () => Array.from(
+  { length: PICTURES_COUNT },
   (_, pictureIndex) => createPicture(pictureIndex + 1),
 );
--- js/util.js
+++ js/util.js
@@ -1,12 +1,20 @@
-const getRandomInteger = (a, b) => {
+export const createIdGenerator = () => {
+  let lastGenerateId = 0;
+
+  return () => {
+    lastGenerateId++;
+    return lastGenerateId;
+  };
+};
+
+export const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
+
   return Math.floor(result);
 };
 
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-
-export {getRandomInteger, getRandomArrayElement};
+export const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 export const isEscapeKey = (evt) => evt.key === 'Escape';
